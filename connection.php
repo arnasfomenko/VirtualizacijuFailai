e7c47db994144db422ff7d80b50a309cd1e1775a
chlukas <lukas.choromanskis@gmail.com>
2017-12-21 06:20:15 +0200

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+	$source = file_get_contents("dbIP.txt");
+	$link = mysqli_connect($source, "root", "root", "db_name");
+	/* check connection */
+	if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
