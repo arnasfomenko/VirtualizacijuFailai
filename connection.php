6015cfe9db1ffc7c0763c0bb056435c11a095a65
chlukas <lukas.choromanskis@gmail.com>
2017-12-21 06:16:28 +0200

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$source = file_get_contents("dbIP.txt");
-	$link = mysqli_connect($source, "root", "root", "db_name");
-	/* check connection */
-	if (mysqli_connect_errno()) {
-		printf("Connect failed: %s\n", mysqli_connect_error());
-		exit();
-	}
